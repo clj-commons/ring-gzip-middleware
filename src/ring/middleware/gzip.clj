534d2da9f5f5d7408bd57d94100b4d7bbe711321
Michael Stephens <michaeljste@gmail.com>
2010-09-14 04:21:14 -0400

    support InputStream body

--- src/ring/middleware/gzip.clj
+++ src/ring/middleware/gzip.clj
@@ -1,22 +1,27 @@
 (ns ring.middleware.gzip
+  (:require [clojure.contrib.io :as io])
   (:import (java.util.zip GZIPOutputStream)
-           (java.io ByteArrayInputStream ByteArrayOutputStream)))
+           (java.io InputStream ByteArrayInputStream ByteArrayOutputStream)))
 
 (defn gzipped-response [resp]
-  (let [bout (ByteArrayOutputStream.)
+  (let [body (resp :body)
+        bout (ByteArrayOutputStream.)
         out (GZIPOutputStream. bout)
         headers (assoc (resp :headers) "content-encoding" "gzip")]
-    (.write out (.getBytes (resp :body)))
+    (io/copy (resp :body) out)
     (.close out)
+    (if (instance? InputStream body) (.close body))
     (merge resp {:body (ByteArrayInputStream. (.toByteArray bout))
                  :headers headers})))
 
 (defn wrap-gzip [handler]
   (fn [req]
-    (let [resp (handler req)]
+    (let [resp (handler req)
+          body (resp :body)]
       (if (and (not ((get resp :headers {}) "content-encoding"))
-               (string? (resp :body))
-               (> (count (resp :body)) 200))
+               (or
+                (and (string? body) (> (count body) 200))
+                (instance? InputStream body)))
         (let [accepts (get (req :headers) "accept-encoding" "")
               match (re-find #"(gzip|\*)(;q=((0|1)(.\d+)?))?" accepts)]
           (if (and match (not (contains? #{"0" "0.0" "0.00" "0.000"}
