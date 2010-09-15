25987d79acb12d25e5a313bd8d4c6c5d214ed637
Michael Stephens <michaeljste@gmail.com>
2010-09-15 19:17:53 -0400

    File support, cleanup

--- src/ring/middleware/gzip.clj
+++ src/ring/middleware/gzip.clj
@@ -1,29 +1,34 @@
 (ns ring.middleware.gzip
   (:require [clojure.contrib.io :as io])
   (:import (java.util.zip GZIPOutputStream)
-           (java.io InputStream ByteArrayInputStream ByteArrayOutputStream)))
+           (java.io InputStream
+                    File
+                    ByteArrayInputStream
+                    ByteArrayOutputStream)))
 
 (defn gzipped-response [resp]
   (let [body (resp :body)
         bout (ByteArrayOutputStream.)
         out (GZIPOutputStream. bout)
-        headers (assoc (resp :headers) "content-encoding" "gzip")]
+        resp (assoc-in resp [:headers "content-encoding"] "gzip")]
     (io/copy (resp :body) out)
     (.close out)
-    (if (instance? InputStream body) (.close body))
-    (merge resp {:body (ByteArrayInputStream. (.toByteArray bout))
-                 :headers headers})))
+    (if (instance? InputStream body)
+      (.close body))
+    (assoc resp :body (ByteArrayInputStream. (.toByteArray bout)))))
 
 (defn wrap-gzip [handler]
   (fn [req]
-    (let [resp (handler req)
-          body (resp :body)]
-      (if (and (= (resp :status) 200)
-               (not ((get resp :headers {}) "content-encoding"))
+    (let [{body :body
+           status :status
+           :as resp} (handler req)]
+      (if (and (= status 200)
+               (not (get-in resp [:headers "content-encoding"]))
                (or
                 (and (string? body) (> (count body) 200))
-                (instance? InputStream body)))
-        (let [accepts (get (req :headers) "accept-encoding" "")
+                (instance? InputStream body)
+                (instance? File body)))
+        (let [accepts (get-in req [:headers "accept-encoding"] "")
               match (re-find #"(gzip|\*)(;q=((0|1)(.\d+)?))?" accepts)]
           (if (and match (not (contains? #{"0" "0.0" "0.00" "0.000"}
                                          (match 3))))
