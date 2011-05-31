759b0301895eaf367e4eebda6eaddfde2bbb6c14
Alan Malloy <alan@malloys.org>
2011-05-31 16:24:21 -0700

    Release 0.1.0 so 4clojure has something stable to work with

--- project.clj
+++ project.clj
@@ -1,4 +1,3 @@
-(defproject amalloy/ring-gzip-middleware "0.1.0-SNAPSHOT"
+(defproject amalloy/ring-gzip-middleware "0.1.0"
   :description "Ring gzip encoding middleware"
-  :dependencies [[org.clojure/clojure "1.2.1"]
-                 [org.clojure/clojure-contrib "1.2.0"]])
+  :dependencies [[org.clojure/clojure "1.2.1"]])
