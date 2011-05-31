75f074884841a901e519babeee0826dc17984de8
Alan Malloy <alan@malloys.org>
2011-05-31 15:53:07 -0700

    Personalizing my fork

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.mikejs/ring-gzip-middleware "0.1.0-SNAPSHOT"
+(defproject amalloy/ring-gzip-middleware "0.1.0-SNAPSHOT"
   :description "Ring gzip encoding middleware"
-  :dependencies [[org.clojure/clojure "1.2.0"]
+  :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/clojure-contrib "1.2.0"]])
