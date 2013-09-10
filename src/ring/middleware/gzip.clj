6c86e479f653754401c2b8e2136fad856b69a72f
Chas Emerick <chas@cemerick.com>
2013-09-10 06:20:30 -0400

    "fast-start" the delivery of gzip-compressed bodies, and support flushing of GZIPOutputStreams on JDK7+
    GZIPOutputStream.flush() is essentially a no-op under <= JDK6 given
    http://bugs.sun.com/bugdatabase/view_bug.do?bug_id=4813885. We weren't
    calling it before anyway, but its JDK7 semantics are necessary when
    compressing seq response bodies; otherwise, the middleware will require
    complete consumption of the seq before a single byte hits the wire.
    Without this change, gzipping of seq bodies that are truly lazy (e.g.
    supporting server-sent events) is not practical; this patch therefore
    also disables seq body support given pre-JDK7 GZIPOutputStream.flush()
    functionality.

--- src/ring/middleware/gzip.clj
+++ src/ring/middleware/gzip.clj
@@ -1,22 +1,54 @@
 (ns ring.middleware.gzip
-  (:require [clojure.java.io :as io])
+  (:require [clojure.java.io :as io]
+            clojure.reflect)
   (:import (java.util.zip GZIPOutputStream)
            (java.io InputStream
+                    OutputStream
                     Closeable
                     File
                     PipedInputStream
                     PipedOutputStream)))
 
+; only available on JDK7
+(def ^:private flushable-gzip?
+  (->> (clojure.reflect/reflect GZIPOutputStream)
+    :members
+    (some (comp '#{[java.io.OutputStream boolean]} :parameter-types))))
+
+; only proxying here so we can specialize io/copy (which ring uses to transfer
+; InputStream bodies to the servlet response) for reading from the result of
+; piped-gzipped-input-stream
+(defn- piped-gzipped-input-stream*
+  []
+  (proxy [PipedInputStream] []))
+
+; exactly the same as do-copy for [InputStream OutputStream], but
+; flushes the output on every chunk; this allows gzipped content to start
+; flowing to clients ASAP (a reasonable change to ring IMO)
+(defmethod @#'io/do-copy [(class (piped-gzipped-input-stream*)) OutputStream]
+  [^InputStream input ^OutputStream output opts]
+  (let [buffer (make-array Byte/TYPE (or (:buffer-size opts) 1024))]
+    (loop []
+      (let [size (.read input buffer)]
+        (when (pos? size)
+          (do (.write output buffer 0 size)
+              (.flush output)
+              (recur)))))))
+
 (defn piped-gzipped-input-stream [in]
-  (let [pipe-in (PipedInputStream.)
+  (let [pipe-in (piped-gzipped-input-stream*)
         pipe-out (PipedOutputStream. pipe-in)]
     (future                  ; new thread to prevent blocking deadlock
-      (with-open [out (GZIPOutputStream. pipe-out)]
+      (with-open [out (if flushable-gzip?
+                        (GZIPOutputStream. pipe-out true)
+                        (GZIPOutputStream. pipe-out))]
         (if (seq? in)
-          (doseq [string in] (io/copy (str string) out))
+          (doseq [string in]
+            (io/copy (str string) out)
+            (.flush out))
           (io/copy in out)))
       (when (instance? Closeable in)
-        (.close in)))
+        (.close ^Closeable in)))
     pipe-in))
 
 (defn gzipped-response [resp]
@@ -34,7 +66,7 @@
                (not (get-in resp [:headers "Content-Encoding"]))
                (or
                 (and (string? body) (> (count body) 200))
-                (seq? body)
+                (and (seq? body) flushable-gzip?)
                 (instance? InputStream body)
                 (instance? File body)))
         (let [accepts (get-in req [:headers "accept-encoding"] "")
