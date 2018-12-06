b8ae1632361da3a1ff350cdea38228e446724d14
danielcompton <desk@danielcompton.net>
2018-12-06 20:03:00 +1300

    Update supported versions of Clojure

--- project.clj
+++ project.clj
@@ -1,8 +1,12 @@
-(defproject amalloy/ring-gzip-middleware "0.1.3"
+(defproject amalloy/ring-gzip-middleware "0.1.4-SNAPSHOT"
   :description "Ring gzip encoding middleware"
-  :dependencies [[org.clojure/clojure "1.4.0"]]
-  :profiles {:1.2.0 {:dependencies [[org.clojure/clojure "1.2.0"]]}
-             :1.3.0 {:dependencies [[org.clojure/clojure "1.3.0"]]}
-             :1.4.0 {}}
-  :aliases {"all" ["with-profile" "1.2.0:1.3.0:1.4.0"]}
+  :dependencies [[org.clojure/clojure "1.9.0"]]
+  :profiles {:1.10 {:dependencies [[org.clojure/clojure "1.10.0-RC3"]]}
+             :1.9  {}
+             :1.8  {:dependencies [[org.clojure/clojure "1.8.0"]]}
+             :1.7  {:dependencies [[org.clojure/clojure "1.7.0"]]}
+             :1.6  {:dependencies [[org.clojure/clojure "1.6.0"]]}
+             ;; Not required except for benchmarks.
+             }
+  :aliases {"all" ["with-profile" "1.6:1.7:1.8:1.9:1.10"]}
   :min-lein-version "2.0.0")
