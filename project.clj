1216663954521d7ab8d07f137c2d7985d689063b
Paula Gearon <quoll@users.noreply.github.com>
2025-01-08 13:33:24 -0500

    Exposing functions publicly (#13)
    * Extracted the testing and updating of headers into public functions
    
    * Added tests for publicly exposed functions
    
    * Updated to include latest minor releases of Clojure

--- project.clj
+++ project.clj
@@ -2,12 +2,14 @@
   :url "https://github.com/clj-commons/ring-gzip-middleware"
   :description "Ring gzip encoding middleware"
   :dependencies [[org.clojure/clojure "1.9.0"]]
-  :profiles {:1.10 {:dependencies [[org.clojure/clojure "1.10.0-RC3"]]}
+  :profiles {:1.12 {:dependencies [[org.clojure/clojure "1.12.0"]]}
+             :1.11 {:dependencies [[org.clojure/clojure "1.11.4"]]}
+             :1.10 {:dependencies [[org.clojure/clojure "1.10.3"]]}
              :1.9  {}
              :1.8  {:dependencies [[org.clojure/clojure "1.8.0"]]}
              :1.7  {:dependencies [[org.clojure/clojure "1.7.0"]]}
              :1.6  {:dependencies [[org.clojure/clojure "1.6.0"]]}}
   :deploy-repositories [["releases" :clojars]
                         ["snapshots" :clojars]]
-  :aliases {"all" ["with-profile" "1.6:1.7:1.8:1.9:1.10"]}
+  :aliases {"all" ["with-profile" "1.6:1.7:1.8:1.9:1.10:1.11:1.12"]}
   :min-lein-version "2.0.0")
