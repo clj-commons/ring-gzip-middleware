d646c76662596fc9eec48eaec502b6b539083adf
Erik Assum <erik@assum.net>
2025-01-08 19:43:07 +0100

    Update project.clj for auto deploy

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject amalloy/ring-gzip-middleware "0.1.5-SNAPSHOT"
+(defproject org.clj-commons/ring-gzip-middleware (or (System/getenv "PROJECT_VERSION") "0.15")
   :url "https://github.com/clj-commons/ring-gzip-middleware"
   :description "Ring gzip encoding middleware"
   :dependencies [[org.clojure/clojure "1.9.0"]]
@@ -9,7 +9,9 @@
              :1.8  {:dependencies [[org.clojure/clojure "1.8.0"]]}
              :1.7  {:dependencies [[org.clojure/clojure "1.7.0"]]}
              :1.6  {:dependencies [[org.clojure/clojure "1.6.0"]]}}
-  :deploy-repositories [["releases" :clojars]
-                        ["snapshots" :clojars]]
+  :deploy-repositories [["clojars" {:url "https://repo.clojars.org"
+                                    :username :env/clojars_username
+                                    :password :env/clojars_password
+                                    :sign-releases true}]]
   :aliases {"all" ["with-profile" "1.6:1.7:1.8:1.9:1.10:1.11:1.12"]}
   :min-lein-version "2.0.0")
