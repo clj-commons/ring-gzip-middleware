3e66df5b0226cd17d80866d01fbc6fbea15f8165
Erik Assum <erik.assum@ardoq.com>
2025-01-08 19:57:35 +0100

    Use correct credentials

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
-(defproject org.clj-commons/ring-gzip-middleware (or (System/getenv "PROJECT_VERSION") "0.15")
+(defproject org.clj-commons/ring-gzip-middleware
+  (or (System/getenv "PROJECT_VERSION") "0.1.5")
   :url "https://github.com/clj-commons/ring-gzip-middleware"
   :description "Ring gzip encoding middleware"
   :dependencies [[org.clojure/clojure "1.9.0"]]
@@ -11,7 +12,7 @@
              :1.6  {:dependencies [[org.clojure/clojure "1.6.0"]]}}
   :deploy-repositories [["clojars" {:url "https://repo.clojars.org"
                                     :username :env/clojars_username
-                                    :password :env/clojars_password
+                                    :password :env/clojars_org_clj_commons_password
                                     :sign-releases true}]]
   :aliases {"all" ["with-profile" "1.6:1.7:1.8:1.9:1.10:1.11:1.12"]}
   :min-lein-version "2.0.0")
